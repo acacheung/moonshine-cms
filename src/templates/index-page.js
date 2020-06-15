2a18cfabba6035fe457d19f5ea13f5a0c45f2795
manda <acacheung@gmail.com>
2020-06-15 18:16:32 +1200

    Better banner message and styles

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -12,11 +12,28 @@ const IndexPage = () => {
             <img className='hero__logo' src={logo} alt='Moonshine 152 logo' />
             <div className='is-hidden'>Moonshine 152</div>
             <div className='hero__desc'>
-              COVID-19 Updates: Please call Moonshine 152 directly at{' '}
-              <a className='t-link' href='tel:6177524191'>
-                617.752.4191
-              </a>{' '}
-              for patio reservations.
+              <p>WE’RE OPEN!</p>
+              <p>
+                Please call us at{' '}
+                <a className='t-link' href='tel:6177524191'>
+                  617.752.4191
+                </a>{' '}
+                for patio reservations or order delivery online with{' '}
+                <a
+                  className='t-link'
+                  href='https://www.grubhub.com/restaurant/moonshine-152-152-dorchester-ave-boston/504704'
+                >
+                  Grubhub
+                </a>{' '}
+                and{' '}
+                <a
+                  className='t-link'
+                  href='https://www.ubereats.com/en-US/boston/food-delivery/moonshine-152/Z27-jG8CQvGhH_iG70G_1w/'
+                >
+                  Uber Eats
+                </a>
+                .
+              </p>
             </div>
           </div>
         </div>
